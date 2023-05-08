41bec08cdbb371cef09d0577a85fa1e61bfd041b
ma2za <mazzapaolo2019@gmail.com>
2023-05-08 18:43:37 -0300

    🪿 xavier init

--- emotion_classification/src/roberta_emotion/modeling_roberta_emotion.py
+++ emotion_classification/src/roberta_emotion/modeling_roberta_emotion.py
@@ -16,6 +16,9 @@ class RobertaEmotion(PreTrainedModel):
         self.dropout = torch.nn.Dropout(p=0.1)
         self.output = torch.nn.Linear(config.hidden_size, config.num_labels)
 
+        for p in self.output.parameters():
+            torch.nn.init.xavier_normal_(p)
+
     def forward(self, input_ids, labels=None, attention_mask=None):
         model_output = self.backbone(input_ids)
         hidden = model_output.last_hidden_state
