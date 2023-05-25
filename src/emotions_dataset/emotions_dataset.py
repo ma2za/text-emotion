c054d3bfb7b3467343ea661885249647a933cbc9
ma2za <mazzapaolo2019@gmail.com>
2023-05-25 13:49:04 -0300

    🪿 dar-ai/emotion dataset support

--- src/emotions_dataset/emotions_dataset.py
+++ src/emotions_dataset/emotions_dataset.py
@@ -1,10 +1,11 @@
+import json
 import os
 import zipfile
 from typing import List
 
 import datasets
 import pandas as pd
-from datasets import ClassLabel, Value, load_dataset
+from datasets import ClassLabel, Value
 
 _URLS = {
     "go_emotions": {
@@ -18,6 +19,10 @@ _URLS = {
     "daily_dialog": {
         "urls": ["http://yanran.li/files/ijcnlp_dailydialog.zip"],
         "license": "CC BY-NC-SA 4.0"
+    },
+    "emotion": {
+        "data": ["data/data.jsonl.gz"],
+        "license": "educational/research"
     }
 }
 
@@ -79,6 +84,11 @@ class EmotionsDataset(datasets.GeneratorBasedBuilder):
             name="daily_dialog",
             label_classes=_CLASS_NAMES,
             features=["text", "label", "dataset", "license"]
+        ),
+        EmotionsDatasetConfig(
+            name="emotion",
+            label_classes=_CLASS_NAMES,
+            features=["text", "label", "dataset", "license"]
         )
     ]
 
@@ -101,7 +111,7 @@ class EmotionsDataset(datasets.GeneratorBasedBuilder):
         splits = []
         if self.config.name == "all":
             for k, v in _URLS.items():
-                downloaded_files = dl_manager.download_and_extract(v.get("urls"))
+                downloaded_files = dl_manager.download_and_extract(v.get("urls", v.get("data")))
                 splits.append(datasets.SplitGenerator(name=k,
                                                       gen_kwargs={"filepaths": downloaded_files,
                                                                   "dataset": k,
@@ -109,12 +119,40 @@ class EmotionsDataset(datasets.GeneratorBasedBuilder):
         else:
             k = self.config.name
             v = _URLS.get(k)
-            downloaded_files = dl_manager.download_and_extract(v.get("urls"))
+            downloaded_files = dl_manager.download_and_extract(v.get("urls", v.get("data")))
             splits.append(datasets.SplitGenerator(name=k,
                                                   gen_kwargs={"filepaths": downloaded_files,
                                                               "dataset": k,
                                                               "license": v.get("license")}))
         return splits
+
+    def process_daily_dialog(self, filepaths, dataset):
+        # TODO move outside
+        emo_mapping = {0: "no emotion", 1: "anger", 2: "disgust",
+                       3: "fear", 4: "happiness", 5: "sadness", 6: "surprise"}
+        for i, filepath in enumerate(filepaths):
+            if os.path.isdir(filepath):
+                emotions = open(os.path.join(filepath, "ijcnlp_dailydialog/dialogues_emotion.txt"), "r").read()
+                text = open(os.path.join(filepath, "ijcnlp_dailydialog/dialogues_text.txt"), "r").read()
+            else:
+                # TODO check if this can be removed
+                archive = zipfile.ZipFile(filepath, 'r')
+                emotions = archive.open("ijcnlp_dailydialog/dialogues_emotion.txt", "r").read().decode()
+                text = archive.open("ijcnlp_dailydialog/dialogues_text.txt", "r").read().decode()
+            emotions = emotions.split("\n")
+            text = text.split("\n")
+
+            for idx_out, (e, t) in enumerate(zip(emotions, text)):
+                if len(t.strip()) > 0:
+                    cast_emotions = [int(j) for j in e.strip().split(" ")]
+                    cast_dialog = [d.strip() for d in t.split("__eou__") if len(d)]
+                    for idx_in, (ce, ct) in enumerate(zip(cast_emotions, cast_dialog)):
+                        uid = f"daily_dialog_{i}_{idx_out}_{idx_in}"
+                        yield uid, {"text": ct,
+                                    "id": uid,
+                                    "dataset": dataset,
+                                    "license": license,
+                                    "label": emo_mapping[ce]}
 
     def _generate_examples(self, filepaths, dataset, license):
         if dataset == "go_emotions":
@@ -131,33 +169,20 @@ class EmotionsDataset(datasets.GeneratorBasedBuilder):
                                 "license": license,
                                 "label": row[current_classes][row == 1].index.item()}
         elif dataset == "daily_dialog":
-            # TODO move outside
-            emo_mapping = {0: "no emotion", 1: "anger", 2: "disgust",
-                           3: "fear", 4: "happiness", 5: "sadness", 6: "surprise"}
+            for d in self.process_daily_dialog(filepaths, dataset):
+                yield d
+        elif dataset == "emotion":
+            emo_mapping = {0: "sadness", 1: "joy", 2: "love",
+                           3: "anger", 4: "fear", 5: "surprise"}
             for i, filepath in enumerate(filepaths):
-                if os.path.isdir(filepath):
-                    emotions = open(os.path.join(filepath, "ijcnlp_dailydialog/dialogues_emotion.txt"), "r").read()
-                    text = open(os.path.join(filepath, "ijcnlp_dailydialog/dialogues_text.txt"), "r").read()
-                else:
-                    # TODO check if this can be removed
-                    archive = zipfile.ZipFile(filepath, 'r')
-                    emotions = archive.open("ijcnlp_dailydialog/dialogues_emotion.txt", "r").read().decode()
-                    text = archive.open("ijcnlp_dailydialog/dialogues_text.txt", "r").read().decode()
-                emotions = emotions.split("\n")
-                text = text.split("\n")
-
-                for idx_out, (e, t) in enumerate(zip(emotions, text)):
-                    if len(t.strip()) > 0:
-                        cast_emotions = [int(j) for j in e.strip().split(" ")]
-                        cast_dialog = [d.strip() for d in t.split("__eou__") if len(d)]
-                        for idx_in, (ce, ct) in enumerate(zip(cast_emotions, cast_dialog)):
-                            uid = f"daily_dialog_{i}_{idx_out}_{idx_in}"
-                            yield uid, {"text": ct,
-                                        "id": uid,
-                                        "dataset": dataset,
-                                        "license": license,
-                                        "label": emo_mapping[ce]}
-
-
-temp = load_dataset("ma2za/emotions_dataset", name="daily_dialog")
-print()
+                with open(filepath, encoding="utf-8") as f:
+                    for idx, line in enumerate(f):
+                        uid = f"{dataset}_{idx}"
+                        example = json.loads(line)
+                        example.update({
+                            "id": uid,
+                            "dataset": dataset,
+                            "license": license,
+                            "label": emo_mapping[example["label"]]
+                        })
+                        yield uid, example
