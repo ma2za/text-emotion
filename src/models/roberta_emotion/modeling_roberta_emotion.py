3fab8b698e6e18c2c60c6dbd300be2bc92543fae
ma2za <mazzapaolo2019@gmail.com>
2023-05-28 22:37:28 -0300

    🐉 lora

--- src/models/roberta_emotion/modeling_roberta_emotion.py
+++ src/models/roberta_emotion/modeling_roberta_emotion.py
@@ -1,8 +1,35 @@
+from collections import OrderedDict
+from operator import attrgetter
+
 import torch
-from transformers import PreTrainedModel, RobertaModel, RobertaConfig
+from torch import Tensor
+from transformers import PreTrainedModel, RobertaModel
+from transformers import RobertaConfig
 from transformers.modeling_outputs import SequenceClassifierOutput
 
-from .configuration_roberta_emotion import RobertaEmotionConfig
+from configuration_roberta_emotion import RobertaEmotionConfig
+
+
+class LoRA(torch.nn.Linear):
+    def __init__(self, in_features: int, out_features: int, weight: Tensor,
+                 bias: Tensor, alpha: int, r: int):
+        super().__init__(in_features, out_features, bias is not None)
+        self.delta_weight = torch.nn.Sequential(OrderedDict([
+            ("A", torch.nn.Linear(in_features, 8, bias=False)),
+            ("B", torch.nn.Linear(8, out_features, bias=False))
+        ]))
+
+        self.alpha = alpha
+        self.r = r
+
+        torch.nn.init.zeros_(self.delta_weight.B.weight)
+        torch.nn.init.normal_(self.delta_weight.A.weight)
+
+        self.weight = weight
+        self.bias = bias
+
+    def forward(self, input: Tensor) -> Tensor:
+        return super().forward(input) + self.delta_weight(input) * (self.alpha / self.r)
 
 
 class RobertaEmotion(PreTrainedModel):
@@ -19,6 +46,22 @@ class RobertaEmotion(PreTrainedModel):
         self.backbone = RobertaModel.from_pretrained("roberta-base",
                                                      kwargs.get("pooling", True),
                                                      config=roberta_base_config)
+
+        if len(kwargs.get("lora", [])) != 0:
+            for name, module in self.backbone.named_modules():
+                if any([i in name for i in kwargs.get("lora")]):
+                    module_name, attr_name = name.rsplit(".", 1)
+                    module: torch.nn.Module = attrgetter(module_name)(self.backbone)
+                    attr: torch.nn.Linear = attrgetter(name)(self.backbone)
+                    module.__setattr__(attr_name, LoRA(
+                        in_features=attr.in_features,
+                        out_features=attr.out_features,
+                        weight=attr.weight,
+                        bias=attr.bias,
+                        alpha=8,
+                        r=8
+                    ))
+
         self.custom_pooling = None
         if not kwargs.get("pooling", True):
             self.custom_pooling = torch.nn.Sequential(
