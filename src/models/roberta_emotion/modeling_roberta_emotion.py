0522f36a73deb6705cae5d58082ca248bb55eba8
ma2za <mazzapaolo2019@gmail.com>
2023-05-30 14:58:50 -0300

    🪿 remove adapters from model

--- src/models/roberta_emotion/modeling_roberta_emotion.py
+++ src/models/roberta_emotion/modeling_roberta_emotion.py
@@ -1,5 +1,5 @@
 import torch
-from torch_adapters.utils import add_prefix_tuning_embedding, add_lora
+from torch import Tensor
 from transformers import PreTrainedModel, RobertaModel
 from transformers import RobertaConfig
 from transformers.modeling_outputs import SequenceClassifierOutput
@@ -22,30 +22,13 @@ class RobertaEmotion(PreTrainedModel):
                                                      kwargs.get("pooling", True),
                                                      config=roberta_base_config)
 
-        if kwargs.get("prefix_tuning") is not None:
-            prefix_config = kwargs.get("prefix_tuning")
-            add_prefix_tuning_embedding(self.backbone, embeddings=prefix_config.get("embeddings"),
-                                        config={"prefix_length": prefix_config.get("prefix_length"),
-                                                "hidden_rank": prefix_config.get("hidden_rank")})
-        if kwargs.get("lora") is not None:
-            add_lora(self.backbone, kwargs.get("lora"), config={"alpha": 8, "r": 8})
-
-        self.custom_pooling = None
-        if not kwargs.get("pooling", True):
-            self.custom_pooling = torch.nn.Sequential(
-                torch.nn.Linear(config.hidden_size, config.hidden_size),
-                torch.nn.GELU(),
-            )
-
         self.classifier = torch.nn.Sequential(
             torch.nn.Dropout(p=0.1),
             torch.nn.Linear(config.hidden_size, config.num_labels)
         )
 
-    def forward(self, input_ids, labels=None, attention_mask=None):
+    def forward(self, input_ids: Tensor, labels: Tensor = None, attention_mask: Tensor = None):
         hidden = self.backbone(input_ids, attention_mask=attention_mask).last_hidden_state[:, 0, :]
-        if self.custom_pooling is not None:
-            hidden = self.custom_pooling(hidden)
         logits = self.classifier(hidden)
 
         loss = None
